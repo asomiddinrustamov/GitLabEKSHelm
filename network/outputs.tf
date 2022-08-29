2b09710c020aab774c32e1f91f8a04b308fff6ac
Adam Rustam <asomiddin@live.com>
2022-08-29 16:54:29 -0400

    added outputs to network

--- network/outputs.tf
+++ network/outputs.tf
@@ -5,3 +5,19 @@ output "nat1_ip" {
 output "nat2_ip" {
     value = aws_eip.nat["public-devops-2"].public_ip
 }
+
+output "vpc_id" {
+    value = aws_vpc.devops.id
+}
+
+output "eip_nat" {
+    value = aws_eip.nat
+}
+
+output "subnet_private" {
+    value = aws_subnet.private
+}
+
+output "subnet_public" {
+    value = aws_subnet.public
+}
