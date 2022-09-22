1d20e8e70add08eccfeb8c493fe8eb2d4d14a9a8
Adam Rustam <62815294+asomiddinrustamov@users.noreply.github.com>
2022-09-22 11:28:31 -0400

    Update versions.tf

--- gitlab/versions.tf
+++ gitlab/versions.tf
@@ -1,1 +1,33 @@
-../versions.tf
+provider "aws" {
+  region = var.region
+}
+
+provider "kubernetes" {
+  host = var.eks_endpoint
+
+  cluster_ca_certificate = base64decode(
+    var.kubeconfig_cert_auth_data
+  )
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
+    command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host = var.eks_endpoint
+
+    cluster_ca_certificate = base64decode(
+      var.kubeconfig_cert_auth_data
+    )
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
+      command     = "aws"
+    }
+  }
+}
